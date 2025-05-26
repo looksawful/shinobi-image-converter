6331ef96c182976f7b2fab47d45360b2147e0263
looksawful <i@lookawful.ru>
2025-05-26 11:41:03 +0300

    deployment fixes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'node:path';
+// frontend/vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+
 export default defineConfig({
-	plugins: [react()],
-	base: '/shinobi-image-converter/',
-	server: { port: 5173 },
-	resolve: {
-		alias: {
-			'cropperjs/dist/cropper.css': path.resolve(__dirname, 'node_modules/cropperjs/dist/cropper.css'),
-		},
-	},
-	build: {
-		outDir: '../docs',
-	},
-});
+  plugins: [react()],
+  base: '/shinobi-image-converter/',
+  server: { port: 5173 },
+  resolve: {
+    alias: {
+      'cropperjs/dist/cropper.css': path.resolve(
+        __dirname,
+        'node_modules/cropperjs/dist/cropper.css'
+      )
+    }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
